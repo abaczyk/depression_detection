4e619eb73be06932b5bea07dd66a0684d0bb29ce
Anna Bączyk <s180489@student.pg.edu.pl>
2023-05-25 11:15:09 +0200

    Deleted unnecessary features

--- feature_extraction.py
+++ feature_extraction.py
@@ -26,22 +26,17 @@ def get_features(wav_path):
     audio = audio_manipulations.remove_noise(wav_path)
     sample_rate = config.SAMPLE_RATE
     features = np.empty((0))
-    # melspec = librosa.feature.melspectrogram(y=audio, n_mels=80, sr = sample_rate).astype(np.float32).T
-    # melspec = np.log(np.maximum(1e-6, melspec))
-    # mel_feature_size = melspec.shape[1]
-    # mel_max_samples = melspec.shape[0]
-    # mel_output_dim = cluster_size * 16
-    # mel_features = (mel_feature_size, mel_max_samples, cluster_size, mel_output_dim)
 
     # f0min = librosa.note_to_hz('C2') #todo - jeśli odpalimy z f0, to strasznie długo to się wykonuje
     # f0max = librosa.note_to_hz('C7')
     # f0_val, voiced_flag, voiced_probs = librosa.pyin(audio, fmin=f0min, fmax=f0max)
-    # features = np.hstack((features, f0_val)
- 
+    # features = np.hstack((features, f0_val))
+    # jitter_val = np.mean(np.abs(np.diff(f0_val[voiced_flag])))
+    # features = np.hstack((features, jitter_val))
+    
     # f0_mean = np.mean(f0_val)
     # features = np.hstack([features, f0_mean])
     
-    # mfcc_melspec = librosa.feature.melspectrogram(y=audio, sr=sample_rate, n_mels=80)
     mfcc_val = np.mean(librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40).T, axis=0)
     features = np.hstack((features, mfcc_val))
 
@@ -59,12 +54,8 @@ def get_features(wav_path):
     speech_rate = get_speech_rate(audio, sample_rate)
     features = np.hstack((features, speech_rate))
 
-    # flux = librosa.onset.onset_strength(y=audio, sr=sample_rate)
     centroid_val = np.mean(librosa.feature.spectral_centroid(y=audio, sr=sample_rate).T, axis=0)
     features = np.hstack((features, centroid_val))
-
-    # bandwidth_val = librosa.feature.spectral_bandwidth(y=audio, sr=sample_rate)
-    # contrast_val = librosa.feature.spectral_contrast(y=audio, sr=sample_rate)
     
     flatness_val = np.mean(librosa.feature.spectral_flatness(y=audio).T, axis=0)
     features = np.hstack((features, flatness_val))
